6a64798c2df48b50e7c5f30fff0873d332673222
Alessandro Fraioli <103855969+peter732@users.noreply.github.com>
2024-04-16 10:48:30 +0200

    Add files via upload

---
+++ ex_1.2.py
@@ -0,0 +1,14 @@
+# Given variables
+length = 10
+width = 5
+
+# Calculate area and perimeter
+area = length * width
+perimeter = 2 * (length + width)
+
+# Print the results in a formatted string
+print("Rectangle Details:")
+print("Length: {}".format(length))
+print("Width: {}".format(width))
+print("Area: {}".format(area))
+print("Perimeter: {}".format(perimeter))
---
+++ ex_1.py
@@ -0,0 +1,3 @@
+age = 20
+name = "Alex"
+print("My name is {name} and I am {age} years old.".format(name=name, age=age))
